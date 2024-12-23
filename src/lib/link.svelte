8c5bff2b070006f561f6b2578f286527767129e6
Daniel Schiemann <daniel.schiemann@gebit.de>
2024-12-23 15:38:48 +0100

    DINF-998: add tooltip to edit link

--- src/lib/link.svelte
+++ src/lib/link.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   export let to: string;
+  export let tooltip: string | undefined = undefined;
 </script>
 
-<a class="text-cyan-700 hover:underline decoration-1" href={to}>
+<a class="text-cyan-700 hover:underline decoration-1" href={to} title={tooltip}>
   <slot />
 </a>
